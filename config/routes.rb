c5cc6e9294ee709de41ae1400791d1f535539f34
Diego Bassan <bsn.diego@gmail.com>
2018-04-25 17:56:57 -0300

    Rotas de Registro de Usuário removidas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :user_types
   get 'welcome_user/index'
-  devise_for :users
+  devise_for :users , :skip => [:registrations]
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
   root 'welcome_user#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
